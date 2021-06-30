d2a1b90cde05acea188077692e582c8df7d76744
Nguyễn Thái Sơn <60878188+solatin@users.noreply.github.com>
2021-06-30 09:20:11 +0700

    [7] [Admin] Quản lý category
    merge template with hbs, display list category
    intergrate api
    add not found route
    finish add, edit
    add formatter file, required install css-html-formatter extension in vscode
    
    Co-authored-by: sonnt <sonnt@hls.asia>

--- middlewares/passport.mdw.js
+++ middlewares/passport.mdw.js
@@ -1,42 +1,52 @@
-const passport = require('passport');
-const passportLocal = require('passport-local');
+const passport = require("passport");
+const passportLocal = require("passport-local");
 const localStrategy = passportLocal.Strategy;
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const accountModel = require('../models/account.model');
+const accountModel = require("../models/account.model");
 
-module.exports = function(app){
-    passport.use(
-        new localStrategy({ usernameField: 'txtEmail',
-                            passwordField: 'txtPassword',
-                            passReqToCallback : true
-        }, async function(req, email, password, done){
-            const account = await accountModel.findByEmail(email);
-            if(!account){
-                return done(null, false, req.flash('error_msg', 'Your Email or Password not correct'));
-            }
-            
-            const match = await bcrypt.compare(password, account.Password);
+module.exports = function (app) {
+  passport.use(
+    new localStrategy(
+      {
+        usernameField: "txtEmail",
+        passwordField: "txtPassword",
+        passReqToCallback: true,
+      },
+      async function (req, email, password, done) {
+        const account = await accountModel.findByEmail(email);
+        if (!account) {
+          return done(
+            null,
+            false,
+            req.flash("error_msg", "Your Email or Password not correct")
+          );
+        }
 
-            if(match){
-                return done(null, account);
-            }
-            else{
-                return done(null ,false, req.flash('error_msg', 'Your Email or Password not correct'));
-            }
-        })
-    );
+        const match = await bcrypt.compare(password, account.Password);
 
-    passport.serializeUser(function(account, done) {
-        done(null, account.ID)
-    })
-    
-    passport.deserializeUser(async function(id, done) {
-        const res = await accountModel.findByID(id);
-        done(null, res);
-    });
+        if (match) {
+          return done(null, account);
+        } else {
+          return done(
+            null,
+            false,
+            req.flash("error_msg", "Your Email or Password not correct")
+          );
+        }
+      }
+    )
+  );
 
-    app.use(passport.initialize());
-    app.use(passport.session());
-}
+  passport.serializeUser(function (account, done) {
+    done(null, account.ID);
+  });
 
+  passport.deserializeUser(async function (id, done) {
+    const res = await accountModel.findByID(id);
+    done(null, res);
+  });
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
