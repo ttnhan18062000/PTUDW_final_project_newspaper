90d3ba043c40f415b4069f1b88f44fd98d2f2608
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2021-07-01 23:00:38 +0700

    Fix bug 3

--- middlewares/locals.mdw.js
+++ middlewares/locals.mdw.js
@@ -1,15 +1,50 @@
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+const categoryModel = require("../models/category.model");
+
+module.exports = function (app) {
+  app.use(async function (req, res, next) {
     if (typeof req.session.auth === "undefined") {
       req.session.auth = false;
     }
 
+    const list = await categoryModel.all();
+    const listCategories = [];
+    const listParentCategories = [];
+
+    let count = 7;
+    list.forEach((parent) => {
+      if (parent.ParentCategoryID === null) {
+        const temp = [];
+        list.forEach((child) => {
+          if (child.ParentCategoryID === parent.ID) {
+            const newChild = {
+              ID: child.ID,
+              Name: child.Name,
+              listChildCategories: null,
+            };
+            temp.push(newChild);
+          }
+        });
+        const newParent = {
+          ID: parent.ID,
+          Name: parent.Name,
+          listChildCategories: temp,
+        };
+        listCategories.push(newParent);
+        if (count > 0) {
+          listParentCategories.push({ ID: parent.ID, Name: parent.Name });
+          count = count - 1;
+        }
+      }
+    });
+
     res.locals.auth = req.session.auth;
     res.locals.authUser = req.session.authUser;
+    res.locals.listParentCategories = listParentCategories;
+    res.locals.listCategories = listCategories;
     next();
   });
 
-  app.use(async(req, res, next) => {
+  app.use(async (req, res, next) => {
     const url = req.originalUrl;
     if (url.startsWith("/admin")) {
       res.locals.lcNavItems = getNavItems(url);
@@ -18,23 +53,23 @@ module.exports = function(app) {
   });
 };
 
-const getNavItems = function(url) {
+const getNavItems = function (url) {
   let navItems = [{
-      name: "Dashboard",
-      href: "/admin/dashboard",
-      icon: "icon-dashboard",
-      class: "",
-    },
-    {
-      name: "Categories",
-      href: "/admin/categories",
-      icon: "icon-list-alt",
-      class: "",
-    },
+    name: "Dashboard",
+    href: "/admin/dashboard",
+    icon: "icon-dashboard",
+    class: "",
+  },
+  {
+    name: "Categories",
+    href: "/admin/categories",
+    icon: "icon-list-alt",
+    class: "",
+  },
   ];
   const itemName = url.split("/")[2];
   navItems = navItems.map((item) =>
-    item.name.toLowerCase() === itemName ? {...item, class: "active" } : item
+    item.name.toLowerCase() === itemName ? { ...item, class: "active" } : item
   );
   return navItems;
 };
--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -2,51 +2,20 @@ const categoryModel = require("../models/category.model");
 
 module.exports = function(app) {
   app.get("/", async function(req, res) {
-    const list = await categoryModel.all();
-    const listCategories = [];
-    const listParentCategories = [];
-
-    let count = 7;
-    list.forEach((parent) => {
-      if (parent.ParentCategoryID === null) {
-        const temp = [];
-        list.forEach((child) => {
-          if (child.ParentCategoryID === parent.ID) {
-            const newChild = {
-              ID: child.ID,
-              Name: child.Name,
-              listChildCategories: null,
-            };
-            temp.push(newChild);
-          }
-        });
-        const newParent = {
-          ID: parent.ID,
-          Name: parent.Name,
-          listChildCategories: temp,
-        };
-        listCategories.push(newParent);
-        if (count > 0) {
-          listParentCategories.push({ ID: parent.ID, Name: parent.Name });
-          count = count - 1;
-        }
-      }
-    });
-
     if (req.session.loginState) {
       const state = req.session.loginState;
       if (state.failed) {
         return res.render('home', {
           showModal: true,
           message: state.message,
-          listCategories: listCategories,
-          listParentCategories: listParentCategories
+          listCategories: res.locals.listCategories,
+          listParentCategories: res.locals.listParentCategories
         });
       }
       if (state.success) {
         return res.render('home', {
-          listCategories: listCategories,
-          listParentCategories: listParentCategories
+          listCategories: res.locals.listCategories,
+          listParentCategories: res.locals.listParentCategories
         });
       }
     }
@@ -54,14 +23,14 @@ module.exports = function(app) {
       return res.render('home', {
         showModal: true,
         message: "You need to login to view this resource",
-        listCategories: listCategories,
-        listParentCategories: listParentCategories
+        listCategories: res.locals.listCategories,
+        listParentCategories: res.locals.listParentCategories
       });
     }
 
     res.render("home", {
-      listCategories: listCategories,
-      listParentCategories: listParentCategories,
+      listCategories: res.locals.listCategories,
+      listParentCategories: res.locals.listParentCategories,
     });
   });
 
