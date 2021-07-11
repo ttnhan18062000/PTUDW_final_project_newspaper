465c8785ce1694b0c981bede6d6e5985c90941cc
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2021-07-11 19:29:42 +0700

    13_Category add link to every post

--- controllers/category.route.js
+++ controllers/category.route.js
@@ -6,34 +6,36 @@ const postModel = require('../models/post.model');
 const tagModel = require('../models/tag.model');
 
 
-router.get("/:categoryID/page:id", async function(req, res){
+router.get("/:categoryID/page:id", async function (req, res) {
     const categoryId = +req.params.categoryID || 0;
     const categoryName = await categoryModel.findById(categoryId);
     console.log(categoryName);
     const pageID = +req.params.id || 0;
     const listPost = await postModel.findPostByCategoryAndPage(categoryId, pageID);
     const top5Post = await postModel.getTop5ViewCountByCategoryId(categoryId);
-    const top4Post =[];
+    const top4Post = [];
     const listPostWithTag = [];
     const numPage = await postModel.getNumPageByCategoryID(categoryId);
-    for (let index = 1; index < 4; index++) {
-        const element = top5Post[index];
-        top4Post.push({
-            Title: element.Title,
-            PublishDate: element.PublishDate,
-            PostID: element.PostID,
-            index: index
-        });
-    }
+    if (listPost !== null) {
+        for (let index = 1; index < top5Post.length - 1; index++) {
+            const element = top5Post[index];
+            top4Post.push({
+                Title: element.Title,
+                PublishDate: element.PublishDate,
+                PostID: element.PostID,
+                index: index
+            });
+        }
 
-    for (let index = 0; index < listPost.length; index++) {
-        const element = listPost[index];
-        const listTag = await tagModel.getAllTagRelatedPost(element.PostID);
-        console.log(element);
-        listPostWithTag.push({
-            Post: element,
-            listTag: listTag,
-        })
+        for (let index = 0; index < listPost.length; index++) {
+            const element = listPost[index];
+            const listTag = await tagModel.getAllTagRelatedPost(element.PostID);
+            console.log(element);
+            listPostWithTag.push({
+                Post: element,
+                listTag: listTag,
+            })
+        }
     }
 
     const paging = [];
@@ -41,16 +43,20 @@ router.get("/:categoryID/page:id", async function(req, res){
         paging.push({
             id: index,
             isCurrentPage: index == pageID,
+            categoryId: categoryId,
         })
     }
     console.log(paging);
+    
     res.render("../views/vmCategory/categoryPage.hbs", {
+        isEmpty: listPost === null,
         isCategoryList: true,
         listPostWithTag: listPostWithTag,
         top4Post: top4Post,
         top5Post: top5Post,
         paging: paging,
         categoryName: categoryName,
+        categoryId: categoryId,
     });
 })
 
