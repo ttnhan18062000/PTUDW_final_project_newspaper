b1026d0f9dd6cea81500061f89dc3a5c21ad4914
Nguyễn Thái Sơn <60878188+solatin@users.noreply.github.com>
2021-07-25 21:11:37 +0700

    [28] [PV] Đăng bài viết

--- configs/post.cfg.js
+++ configs/post.cfg.js
@@ -1,17 +1,20 @@
 const ENDPOINTS = {
   all: `Call GTT_PostDetail();`,
   findByID: (id) => `Call GTR_PostDetail_By_ID(${id});`,
-  add: (name) => `Call INS_Tag('${name}');`,
   getPostTags: (id) => `Call GTT_PostTag_By_PostID(${id});`,
   update: (post) => 
-              `Call UPD_Post('${post.postID}', '${post.writer}', '${post.category}', '${post.premium}', '${post.title}', '${post.abstract}', '${post.content}');`,
+              `Call UPD_Post('${post.postID}', '${post.writer}', '${post.category}', '${post.premium}', 
+              '${post.title}', '${post.abstract}', '${post.content}');`,
   delete: (id) => `Call DEL_Cascade_Post(${id});`,
-  create: (post) => `Call INS_PostDetail('${post.writer}', '${post.category}', '${post.premium}', '${post.status}', '${post.publishDate}', '${post.title}', '${post.abstract}', '${post.content}');`,
+  createByAdmin: (post) => `Call INS_PostDetail('${post.writer}', '${post.category}', '${post.premium}', 
+    '${post.status}', '${post.publishDate}', '${post.title}', '${post.abstract}', '${post.content}');`,
   insertTag: (postID, tagID) => `Call INS_PostTag('${postID}', '${tagID}')`,
   deleteTag: (postID, tagID) => `Call DEL_PostTag('${postID}', '${tagID}')`,
   publish: (postID, publishDate) => `Call UPD_Post_Publish('${postID}', '${publishDate}')`,
   updatePublishDate: (postID, publishDate) => `Call UPD_Post_PublishDate('${postID}', '${publishDate}')`,
-  getPostByWriterID: (id) => `Call GTT_PostDetail_By_Writer(${id})`
+  getPostByWriterID: (id) => `Call GTT_PostDetail_By_Writer(${id})`,
+  createByWriter: (post) => `Call INS_PostDetail('${post.writer}', '${post.category}', '${post.premium}', 
+  'Draft', NULL, '${post.title}', '${post.abstract}', '${post.content}');`,
 }
 module.exports = {
   ENDPOINTS,
