c5047d2d814ab89e68a58e04025df0b784a9acef
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2021-08-01 13:39:33 +0700

    24_Fix responsive post

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -68,6 +68,7 @@ module.exports = function (app) {
       }
       count = count + 1;
     });
+    console.log(list10PostPerCatReturn);
     
     if (req.session.loginState) {
       const state = req.session.loginState;
