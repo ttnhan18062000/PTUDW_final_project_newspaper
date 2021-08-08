e57ab6977cce077aea806c07889d3fbaa6f0070a
Trần Tuấn Đạt <62866371+bobyba2000@users.noreply.github.com>
2021-08-08 19:16:04 +0700

    [26] [DG] Các thao tác với bài viết premium

--- controllers/post.route.js
+++ controllers/post.route.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const postModel = require("../models/post.model");
 const tagModel = require("../models/tag.model");
+const commentModel = require("../models/comment.model");
+const moment = require('moment');
 const router = express.Router();
 
-router.get('/:id', async function(req, res){
+router.get('/:id', async function (req, res) {
+    var accountID = -1;
+    var accountName = null;
+    var hasAccount = false;
+    var isPremium = false;
+    console.log(req.session.account);
+    if (req.session.account != null) {
+        accountID = req.session.account.ID;
+        accountName = req.session.account.DisplayName;
+        hasAccount = true;
+        isPremium = req.session.account.PremiumStatus != 0;
+    }
+
     const postId = +req.params.id || 0;
+    await postModel.increaseViewCountBy1(postId);
     const post = await postModel.findById(postId);
+    post.PublishDate = moment(post.PublishDate).format("YYYY-MM-DD HH:mm:ss");
     const top5RelatedPost = await postModel.find5PostRelatedById(postId);
     const listTag = await tagModel.getAllTagRelatedPost(postId);
+    var listComment = await commentModel.getCommentByPost(postId);
+    if (listComment !== null) {
+        listComment.forEach(comment => {
+            comment.Date = moment(comment.Date).local().format("YYYY-MM-DD HH:mm:ss");
+        });
+        console.log(listComment);
+        listComment = listComment.reverse();
+    }
+
     res.render('vmPost/postDetail', {
         post: post,
         top5RelatedPost: top5RelatedPost,
         listTag: listTag,
+        accountID: accountID,
+        accountName: accountName,
+        listComment: listComment,
+        hasAccount: hasAccount,
+        isPremium: isPremium,
     });
+})
+
+router.post("/addComment", async function (req, res) {
+    const { PostID, AccountID, DateTime, Content } = req.body;
+    commentModel.insComment(PostID, AccountID, DateTime, Content);
 })
 
 module.exports = router;
