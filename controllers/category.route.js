a048f36f5d0d0def41ddbb262e4f89d68869c587
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2021-08-17 20:35:47 +0700

    Fix wrong number

--- controllers/category.route.js
+++ controllers/category.route.js
@@ -37,7 +37,7 @@ router.get("/:categoryID", async function (req, res) {
     }
     const paging = [];
     var pId = pageID - 1;
-    var num = +numPage[0].Total / 5;
+    var num = Math.round(+numPage[0].Total / 5 + 0.5);
     if(num > pId + 5)
         num = pId + 5;
     if(num - pId < 5){
