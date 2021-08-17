a6ec9315e14d16e0500b4a100a8f4f128b14c245
solatin <nguyenthaison164hv@gmail.com>
2021-08-17 20:42:08 +0700

    xoa log hihi

--- controllers/tag.route.js
+++ controllers/tag.route.js
@@ -37,7 +37,6 @@ router.get("/:tagID", async function (req, res) {
 
     const paging = [];
     var pId = pageID - 1;
-    console.log(numPage)
     var num = Math.round(+numPage.Total / 5 + 0.5);
     if(num > pId + 5)
         num = pId + 5;
